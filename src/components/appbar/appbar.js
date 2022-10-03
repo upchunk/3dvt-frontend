e1b0dcc9621e0d073c9695ef48a2a53ab940bd6f
Habibul Rahman Qalbi <habibul.rahman@lndata.com>
2022-10-03 21:20:15 +0700

    feat: :sparkles: add Dummy Text

--- src/components/appbar/appbar.js
+++ src/components/appbar/appbar.js
@@ -26,21 +26,14 @@ import {
 const drawerWidth = 0;
 
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-  ({ theme, open }) => ({
+  ({ theme }) => ({
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: 0,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
+    marginLeft: 0,
   })
 );
 
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,20 +1,41 @@
 import React from "react";
 import "./landing.css";
+import logo from "../../assets/Logo Horizontal Crop.png";
+import background from "../../assets/background.svg";
+
+import { faker } from "@faker-js/faker";
+const judul1 = faker.lorem.sentence(4);
+const judul2 = faker.lorem.sentence(4);
+const judul3 = faker.lorem.sentence(4);
+const judul4 = faker.lorem.sentence(4);
+const paragraph1 = faker.lorem.paragraph(5);
+const paragraph2 = faker.lorem.paragraph(5);
+const paragraph3 = faker.lorem.paragraph(5);
+const paragraph4 = faker.lorem.paragraph(5);
 
 export default function Landing() {
   return (
-    <div className="landing">
-      <section>
-        <h1 className="textRow">Coming Soon</h1>
+    <div>
+      <img className="background" src={background}></img>
+      <section className="flex-container">
+        <img className="app-logo" src={logo} width={"40%"}></img>
+        <h1 className="textRow">{judul1}</h1>
+        <p className="textRow">{paragraph1}</p>
       </section>
-      <section>
-        <h1 className="textRow">Coming Soon</h1>
+      <section className="flex-container">
+        <img className="app-logo" src={logo} width={400}></img>
+        <h1 className="textRow">{judul2}</h1>
+        <p className="textRow">{paragraph2}</p>
       </section>
-      <section>
-        <h1 className="textRow">Coming Soon</h1>
+      <section className="flex-container">
+        <img className="app-logo" src={logo} width={400}></img>
+        <h1 className="textRow">{judul3}</h1>
+        <p className="textRow">{paragraph3}</p>
       </section>
-      <section>
-        <h1 className="textRow">Coming Soon</h1>
+      <section className="flex-container">
+        <img className="app-logo" src={logo} width={400}></img>
+        <h1 className="textRow">{judul4}</h1>
+        <p className="textRow">{paragraph4}</p>
       </section>
     </div>
   );
