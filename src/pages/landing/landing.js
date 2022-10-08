d11ed23f7f559dc2ab04c6440de2f5cd0f2f9626
Habibul Rahman Qalbi <habibul.rahman@lndata.com>
2022-10-08 23:26:24 +0700

    feat: :sparkles: add publicaton section

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -3,26 +3,31 @@ import "./landing.css";
 import logo from "../../assets/Logo Horizontal Crop.png";
 import ilust1 from "../../assets/ilust1.svg";
 import ilust2 from "../../assets/ilust2.svg";
+import ilust3 from "../../assets/ilust3.svg";
 import { faker } from "@faker-js/faker";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, CssBaseline, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/system";
 import Platform from "../../components/platformCard";
+import { BiRightArrowAlt } from "react-icons/bi";
 
 export default function Landing() {
   const judul1 = "Selamat datang di 3DVT";
   const judul2 = "Kenapa 3DVT dibuat?";
   const judul3 = "Bagaimana 3DVT Bekerja?";
   const judul4 = "Platform yang tersedia";
+  const judul5 = "Publikasi";
   const paragraph1 = faker.lorem.paragraph(5);
   const paragraph2 = faker.lorem.paragraph(5);
   const paragraph3 = faker.lorem.paragraph(5);
   const paragraph4 = faker.lorem.paragraph(5);
+  const paragraph5 = faker.lorem.paragraph(5);
   const navigate = useNavigate();
 
   return (
-    <Box className="landing flex-container" paddingX={24}>
-      <section className="section1">
+    <Box className="landing" paddingX={24} sx={{ flexGrow: 1 }}>
+      <CssBaseline />
+      <section className="section">
         <Grid
           container
           spacing={12}
@@ -32,10 +37,10 @@ export default function Landing() {
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={10}>
-            <img className="app-logo" src={logo} style={{ maxWidth: 300 }} />
+          <Grid item xs={12}>
+            <img className="app-logo" src={logo} loading="lazy" />
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={12}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
@@ -70,7 +75,7 @@ export default function Landing() {
           </Grid>
         </Grid>
       </section>
-      <section className="section2">
+      <section className="section">
         <Grid
           container
           spacing={12}
@@ -78,10 +83,10 @@ export default function Landing() {
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={10} md={4}>
-            <img src={ilust1} style={{ maxWidth: 400 }}></img>
+          <Grid item xs={12} md={4}>
+            <img src={ilust1} className="ilustration" loading="lazy" />
           </Grid>
-          <Grid item xs={10} md={8}>
+          <Grid item xs={12} md={8}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
@@ -102,7 +107,7 @@ export default function Landing() {
           </Grid>
         </Grid>
       </section>
-      <section className="section3">
+      <section className="section">
         <Grid
           container
           spacing={12}
@@ -110,13 +115,13 @@ export default function Landing() {
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={10} md={8}>
+          <Grid item xs={12} md={8}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
               fontWeight={"bold"}
               align="justify"
-              paddingY={2}
+              padding={2}
             >
               {judul3}
             </Typography>
@@ -124,17 +129,17 @@ export default function Landing() {
               fontFamily="montserrat"
               fontSize={"1.2rem"}
               align="justify"
-              paddingY={2}
+              padding={2}
             >
               {paragraph3}
             </Typography>
           </Grid>
-          <Grid item xs={10} md={4}>
-            <img src={ilust2} style={{ maxWidth: 400 }}></img>
+          <Grid item xs={12} md={4}>
+            <img src={ilust2} className="ilustration" loading="lazy" />
           </Grid>
         </Grid>
       </section>
-      <section className="section4">
+      <section className="section">
         <Grid
           container
           spacing={2}
@@ -142,13 +147,13 @@ export default function Landing() {
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={10}>
+          <Grid item xs={12}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
               fontWeight={"bold"}
               align="center"
-              paddingY={2}
+              padding={2}
             >
               {judul4}
             </Typography>
@@ -156,7 +161,7 @@ export default function Landing() {
               fontFamily="montserrat"
               fontSize={"1.2rem"}
               align="center"
-              paddingY={2}
+              padding={2}
             >
               {paragraph4}
             </Typography>
@@ -165,15 +170,58 @@ export default function Landing() {
               spacing={12}
               alignItems="center"
               justifyContent="center"
-              paddingY={2}
+              padding={2}
             >
-              <Grid item xs={12} md={5} sx={{ paddingX: 3, paddingY: 2 }}>
+              <Grid item xs={12} md={5}>
                 <Platform variant={"Android"} />
               </Grid>
-              <Grid item xs={12} md={5} sx={{ paddingX: 3, paddingY: 2 }}>
+              <Grid item xs={12} md={5}>
                 <Platform variant={"Website"} />
               </Grid>
             </Grid>
+          </Grid>
+        </Grid>
+      </section>
+      <section className="section">
+        <Grid
+          container
+          spacing={12}
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={12} md={8}>
+            <Typography
+              variant="h4"
+              fontFamily="montserrat"
+              fontWeight={"bold"}
+              align="justify"
+              paddingY={2}
+            >
+              {judul5}
+            </Typography>
+            <Typography
+              fontFamily="inherit"
+              fontSize={"1.2rem"}
+              align="justify"
+              paddingY={2}
+            >
+              {paragraph5}
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                width: "content",
+                backgroundColor: "#0148A9",
+                paddingX: 3,
+                align: "center",
+              }}
+            >
+              Kunjungi Jurnal Publikasi <BiRightArrowAlt fontSize={"large"} />
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <img src={ilust3} className="ilustration" loading="lazy" />
           </Grid>
         </Grid>
       </section>
