93e8d49dc22610adfcbb83beb78b6d9f5b8594b8
Habibul Rahman Qalbi <habibul.rahman@lndata.com>
2022-10-11 22:51:37 +0700

    feat: :sparkles: Migrate from Grid to Stack, fix Overlapping Issue

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -5,9 +5,9 @@ import ilust1 from "../../assets/ilust1.svg";
 import ilust2 from "../../assets/ilust2.svg";
 import ilust3 from "../../assets/ilust3.svg";
 import { faker } from "@faker-js/faker";
-import { Button, CssBaseline, Grid, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Box } from "@mui/system";
+import { Stack } from "@mui/system";
 import Platform from "../../components/platformCard";
 import { BiRightArrowAlt } from "react-icons/bi";
 
@@ -25,74 +25,59 @@ export default function Landing() {
   const navigate = useNavigate();
 
   return (
-    <Box className="landing" paddingX={24} sx={{ flexGrow: 1 }}>
-      <CssBaseline />
-      <section className="section">
-        <Grid
-          container
-          spacing={12}
-          rowSpacing={4}
+    <Stack className="landing" paddingX={24} sx={{ flexGrow: 1 }}>
+      <section>
+        <Stack
           direction="column"
           alignItems="center"
           justifyContent="center"
-          style={{ minHeight: "100vh" }}
+          spacing={{ xs: 3 }}
+          sx={{ minHeight: "100vh" }}
         >
-          <Grid item xs={12}>
-            <img className="app-logo" src={logo} loading="lazy" />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography
-              variant="h4"
-              fontFamily="montserrat"
-              fontWeight={"bold"}
-              align="center"
-              paddingY={2}
-            >
-              {judul1}
-            </Typography>
-            <Typography
-              fontFamily="montserrat"
-              fontSize={"1.2rem"}
-              align="center"
-              paddingY={2}
-            >
-              {paragraph1}
-            </Typography>
-          </Grid>
-          <Grid item xs={10}>
-            <Button
-              variant="contained"
-              onClick={() => navigate("../masuk")}
-              sx={{
-                width: "content",
-                backgroundColor: "#0148A9",
-                paddingX: 3,
-                align: "center",
-              }}
-            >
-              Coba Sekarang
-            </Button>
-          </Grid>
-        </Grid>
+          <img className="app-logo" src={logo} loading="lazy" />
+          <Typography
+            variant="h4"
+            fontFamily="montserrat"
+            fontWeight={"bold"}
+            align="center"
+          >
+            {judul1}
+          </Typography>
+          <Typography
+            fontFamily="montserrat"
+            fontSize={"1.2rem"}
+            align="center"
+          >
+            {paragraph1}
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={() => navigate("../masuk")}
+            sx={{
+              width: "200px",
+              backgroundColor: "#0148A9",
+              align: "center",
+            }}
+          >
+            Coba Sekarang
+          </Button>
+        </Stack>
       </section>
-      <section className="section">
-        <Grid
-          container
-          spacing={12}
+      <section>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          spacing={{ xs: 2, sm: 2, md: 4 }}
           alignItems="center"
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={12} md={4}>
-            <img src={ilust1} className="ilustration" loading="lazy" />
-          </Grid>
-          <Grid item xs={12} md={8}>
+          <img src={ilust1} className="ilustration" loading="lazy" />
+          <Stack direction={{ xs: "column" }}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
               fontWeight={"bold"}
               align="justify"
-              paddingY={2}
             >
               {judul2}
             </Typography>
@@ -100,28 +85,26 @@ export default function Landing() {
               fontFamily="inherit"
               fontSize={"1.2rem"}
               align="justify"
-              paddingY={2}
             >
               {paragraph2}
             </Typography>
-          </Grid>
-        </Grid>
+          </Stack>
+        </Stack>
       </section>
-      <section className="section">
-        <Grid
-          container
-          spacing={12}
+      <section>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
           alignItems="center"
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={12} md={8}>
+          <Stack direction={{ xs: "column" }}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
               fontWeight={"bold"}
               align="justify"
-              padding={2}
             >
               {judul3}
             </Typography>
@@ -129,74 +112,65 @@ export default function Landing() {
               fontFamily="montserrat"
               fontSize={"1.2rem"}
               align="justify"
-              padding={2}
             >
               {paragraph3}
             </Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <img src={ilust2} className="ilustration" loading="lazy" />
-          </Grid>
-        </Grid>
+          </Stack>
+          <img src={ilust2} className="ilustration" loading="lazy" />
+        </Stack>
       </section>
-      <section className="section">
-        <Grid
-          container
-          spacing={2}
+      <section>
+        <Stack
+          direction={{ xs: "column" }}
+          spacing={{ xs: 2 }}
+          alignItems="center"
+          justifyContent="center"
+          padding={2}
+          style={{ minHeight: "100vh" }}
+        >
+          <Typography
+            variant="h4"
+            fontFamily="montserrat"
+            fontWeight={"bold"}
+            align="center"
+            padding={2}
+          >
+            {judul4}
+          </Typography>
+          <Typography
+            fontFamily="montserrat"
+            fontSize={"1.2rem"}
+            align="center"
+            padding={2}
+          >
+            {paragraph4}
+          </Typography>
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+            alignItems="center"
+            justifyContent="center"
+            padding={2}
+          >
+            <Platform variant={"Android"} />
+            <Platform variant={"Website"} />
+          </Stack>
+        </Stack>
+      </section>
+      <section>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
           alignItems="center"
           justifyContent="center"
           style={{ minHeight: "100vh" }}
         >
-          <Grid item xs={12}>
-            <Typography
-              variant="h4"
-              fontFamily="montserrat"
-              fontWeight={"bold"}
-              align="center"
-              padding={2}
-            >
-              {judul4}
-            </Typography>
-            <Typography
-              fontFamily="montserrat"
-              fontSize={"1.2rem"}
-              align="center"
-              padding={2}
-            >
-              {paragraph4}
-            </Typography>
-            <Grid
-              container
-              spacing={12}
-              alignItems="center"
-              justifyContent="center"
-              padding={2}
-            >
-              <Grid item xs={12} md={5}>
-                <Platform variant={"Android"} />
-              </Grid>
-              <Grid item xs={12} md={5}>
-                <Platform variant={"Website"} />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </section>
-      <section className="section">
-        <Grid
-          container
-          spacing={12}
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Grid item xs={12} md={8}>
+          <Stack direction={{ xs: "column" }} spacing={{ xs: 1, sm: 2, md: 4 }}>
             <Typography
               variant="h4"
               fontFamily="montserrat"
               fontWeight={"bold"}
               align="justify"
-              paddingY={2}
             >
               {judul5}
             </Typography>
@@ -204,27 +178,23 @@ export default function Landing() {
               fontFamily="inherit"
               fontSize={"1.2rem"}
               align="justify"
-              paddingY={2}
             >
               {paragraph5}
             </Typography>
             <Button
               variant="contained"
               sx={{
-                width: "content",
+                width: "fit-content",
                 backgroundColor: "#0148A9",
-                paddingX: 3,
                 align: "center",
               }}
             >
               Kunjungi Jurnal Publikasi <BiRightArrowAlt fontSize={"large"} />
             </Button>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <img src={ilust3} className="ilustration" loading="lazy" />
-          </Grid>
-        </Grid>
+          </Stack>
+          <img src={ilust3} className="ilustration" loading="lazy" />
+        </Stack>
       </section>
-    </Box>
+    </Stack>
   );
 }
