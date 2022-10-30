0d2f33ee2c7922e7f7435c1a81b7558379fdcd68
feratyusa <prabuega55@gmail.com>
2022-10-31 05:01:23 +0700

    feat: apply SSL

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(3dvtusg.com www.3dvtusg.com)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="prabuega55@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
