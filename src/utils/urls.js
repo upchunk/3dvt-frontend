0a923566de95f5f86461ad9adfc6afbdbc5e4aa9
feratyusa <prabuega55@gmail.com>
2022-10-30 18:40:17 +0700

    fix: bad gateway

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-export const baseURL = () => process.env.MODE != 'server' ? `http://localhost:8000` : 'https://localhost:8000';
+export const baseURL = () => process.env.MODE != 'server' ? `http://localhost:8000` : 'https://3dvtusg.com';
 
 export const userListUrl = () => `/api/users/`;
 
